2b20a15c0bea330d0c05689f97985082558522a6
vesk4000 <30286047+vesk4000@users.noreply.github.com>
2018-05-14 12:29:39 +0300

    Add files via upload

---
+++ Local C++ Judge System/Local C++ Judge System/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Local_C___Judge_System
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            //MessageBox.Show(Evaluator.getRoaming());
+        }
+        
+        //An object for the Source File Browser
+        OpenFileDialog ofd = new OpenFileDialog();
+
+        //An object for the Tests Folder Browser
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+        //The function that activates when the G++ Browse
+        private void buttonGPPBrowse_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "g++ Compiler|g++.exe";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBoxGPP.Text = ofd.FileName;
+            }
+        }
+
+        //The function that activates when the Source Browse Button is pressed
+        private void buttonSourceBrowse_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "C++ Source File|*.cpp";//Set the filter's name to "C++ Source File" and the extension to ".cpp"
+            if (ofd.ShowDialog() == DialogResult.OK)//If the user has succesfully selected a cpp file
+            {
+                textBoxSource.Text = ofd.FileName;
+            }
+        }
+
+        //The function that activates when the Tests Browse Button is pressed
+        private void buttonTestsBrowse_Click(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK)//If the user has succesfully selected a folder
+            {
+                textBoxTests.Text = fbd.SelectedPath;
+            }
+        }
+        
+        //The function that activates when the Evaluate Button is pressed
+        private void buttonEvaluate_Click(object sender, EventArgs e)
+        {
+            labelJudgeResult.Text = Convert.ToString(Evaluator.Compile(textBoxSource.Text, Evaluator.cdir + "\\output.exe", 1000000));
+        }
+    }
+}
---
+++ Local C++ Judge System/Local C++ Judge System/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Local_C___Judge_System
+{
+    static class Evaluator
+    {
+        //get the current directory
+        public static string cdir = Directory.GetCurrentDirectory();
+
+        public static bool Compile(string sourcePath, string exePath, int compilationTimeLimit)
+        {
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "CMD";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+            cmd.StandardInput.WriteLine("c:");
+            cmd.StandardInput.WriteLine("cd C:\\MinGW\\bin");
+            cmd.StandardInput.WriteLine("g++ \"E:\\DeKStop\\helloworld.cpp\" -o \"E:\\DeKStop\\helloworld.exe\" -static-libgcc -static-libstdc++");
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+
+            return true; 
+        }
+        public static string getRoaming()
+        {
+            return Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\Roaming";
+        }
+
+        public static void Settings()
+        {
+            string tdir = getRoaming();
+            if (!Directory.Exists(tdir + "\\Vesk"))
+                Directory.CreateDirectory(tdir + "\\Vesk");
+            tdir = tdir + "\\Vesk";
+
+            if(!Directory.Exists(tdir + "\\Local C++ Judge System"))
+                Directory.CreateDirectory(tdir + "\\Local C++ Judge System");
+            tdir = tdir + "\\Local C++ Judge System";
+
+            //Setup settings.ini
+            if(!File.Exists(tdir + "\\settings.ini"))
+            {
+
+            }
+        }
+    }
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
